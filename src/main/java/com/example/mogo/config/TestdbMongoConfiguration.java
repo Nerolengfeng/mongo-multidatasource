e33c069469aa3e6988923676d004b2b3c1f614fa
Nero <xduanshan@126.com>
2019-10-30 10:13:59 +0800

    修改springboot版本为2.2.0

--- src/main/java/com/example/mogo/config/TestdbMongoConfiguration.java
+++ src/main/java/com/example/mogo/config/TestdbMongoConfiguration.java
@@ -1,16 +1,11 @@
 package com.example.mogo.config;
 
-import com.mongodb.MongoClientOptions;
-import com.mongodb.MongoClientURI;
 import lombok.Data;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
-import org.springframework.data.mongodb.MongoDbFactory;
 import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.data.mongodb.core.SimpleMongoDbFactory;
 
 /**
  * @program: mongo
--- src/test/java/com/example/mogo/MogoApplicationTests.java
+++ src/test/java/com/example/mogo/MogoApplicationTests.java
@@ -1,16 +1,31 @@
 package com.example.mogo;
 
+import com.example.mogo.entity.primary.PrimaryMongoObject;
+import com.example.mogo.repository.primary.PrimaryRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Optional;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class MogoApplicationTests {
 
+    @Autowired
+    private PrimaryRepository primaryRepository;
+    
     @Test
     public void contextLoads() {
+        PrimaryMongoObject mongoObject = new PrimaryMongoObject();
+        mongoObject.setValue("hehe");
+        PrimaryMongoObject save = primaryRepository.save(mongoObject);
+        System.out.println(save);
+        Optional<PrimaryMongoObject> byId = primaryRepository.findById(save.getId());
+        System.out.println(byId);
+
     }
 
 }
